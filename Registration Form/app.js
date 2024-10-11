d9474bd14250f63cdeb2c5256e10d8bf5eb0507b
jhajhan <jhannasllovit@iskolarngbayan.pup.edu.ph>
2024-10-11 15:15:09 +0800

    done

---
+++ Registration Form/app.js
@@ -0,0 +1,106 @@
+var cityBarangay = {
+    "Bacoor": [
+        "Alima", "Aniban I", "Aniban II", "Aniban III", "Banalo", "Bayanan", "Campo Santo", "Caviteno", "Digman",
+        "Habay I", "Habay II", "Kaingen", "Ligas I", "Ligas II", "Ligas III", "Mabolo I", "Mabolo II", "Mabolo III",
+        "Molino I", "Molino II", "Molino III", "Molino IV", "Molino V", "Molino VI", "Molino VII", "Mambog I", 
+        "Mambog II", "Mambog III", "Mambog IV", "Mambog V", "Mambog VI", "Niog I", "Niog II", "Niog III", "Panapaan I",
+        "Panapaan II", "Panapaan III", "Panapaan IV", "Panapaan V", "Panapaan VI", "Panapaan VII", "Panapaan VIII",
+        "Real I", "Real II", "Salinas I", "Salinas II", "Salinas III", "Salinas IV", "Talaba I", "Talaba II", 
+        "Talaba III", "Talaba IV", "Talaba V", "Talaba VI", "Talaba VII", "Zapote I", "Zapote II", "Zapote III", 
+        "Zapote IV", "Zapote V"
+    ],
+    "Dasmariñas": [
+        "Burol I", "Burol II", "Burol III", "Datu Esmael", "Emilio Aguinaldo", "Fatima I", "Fatima II", "Langkaan I",
+        "Langkaan II", "Paliparan I", "Paliparan II", "Paliparan III", "Sabang", "Salawag", "Sampaloc I", "Sampaloc II",
+        "Sampaloc III", "Sampaloc IV", "San Agustin I", "San Agustin II", "San Andres I", "San Andres II", 
+        "San Dionisio", "San Esteban", "San Francisco I", "San Francisco II", "San Isidro", "San Jose", "San Juan", 
+        "San Luis I", "San Luis II", "San Manuel I", "San Manuel II", "Santa Cristina I", "Santa Cristina II", 
+        "Victoria Reyes"
+    ],
+    "Imus":  [
+        "Alapan I-A", "Alapan I-B", "Alapan I-C", "Alapan II-A", "Alapan II-B", "Bucandala I", "Bucandala II",
+        "Bucandala III", "Bucandala IV", "Bucandala V", "Carsadang Bago I", "Carsadang Bago II", "Malagasang I-A",
+        "Malagasang I-B", "Malagasang I-C", "Malagasang I-D", "Malagasang I-E", "Malagasang I-F", "Malagasang I-G",
+        "Malagasang II-A", "Malagasang II-B", "Malagasang II-C", "Malagasang II-D", "Malagasang II-E", 
+        "Malagasang II-F", "Malagasang II-G", "Medicion I-A", "Medicion I-B", "Medicion I-C", "Medicion I-D",
+        "Medicion II-A", "Medicion II-B", "Medicion II-C", "Medicion II-D", "Medicion II-E", "Medicion II-F",
+        "Pag-asa I", "Pag-asa II", "Pag-asa III", "Poblacion I-A", "Poblacion I-B", "Poblacion I-C", "Poblacion II-A",
+        "Poblacion II-B", "Poblacion III-A", "Poblacion III-B", "Poblacion IV-A", "Poblacion IV-B", "Poblacion IV-C",
+        "Poblacion IV-D", "Toclong I-A", "Toclong I-B", "Toclong I-C", "Toclong II-A", "Toclong II-B",
+        "Anabu I-A", "Anabu I-B", "Anabu I-C", "Anabu I-D", "Anabu I-E", "Anabu I-F", "Anabu I-G", "Anabu II-A",
+        "Anabu II-B", "Anabu II-C", "Anabu II-D", "Anabu II-E", "Anabu II-F", "Bagong Silang (Bahayang Pag-asa)",
+        "Bayan Luma I", "Bayan Luma II", "Bayan Luma III", "Bayan Luma IV", "Bayan Luma V", "Bayan Luma VI",
+        "Bayan Luma VII", "Bayan Luma VIII", "Bayan Luma IX", "Buhay na Tubig", "Magdalo", "Maharlika", 
+        "Mariano Espeleta I", "Mariano Espeleta II", "Mariano Espeleta III", "Palico I", "Palico II", "Palico III",
+        "Palico IV", "Pasong Buaya I", "Pasong Buaya II", "Pinagbuklod", "Tanzang Luma I", "Tanzang Luma II",
+        "Tanzang Luma III", "Tanzang Luma IV (Southern City)", "Tanzang Luma V", "Tanzang Luma VI"
+    ],
+    "General Trias": [
+        "Arnaldo Poblacion", "Bacao I", "Bacao II", "Bagumbayan", "Buenavista II", "Ciudad Real", "Corregidor",
+        "Governor Ferrer", "Manggahan", "Navarro", "Pasong Kawayan I", "Pasong Kawayan II", "Pinagtipunan", 
+        "San Francisco", "San Juan", "Santiago", "Tapia", "Tejero"
+    ],
+    "Cavite City": [
+        "Barangay 1", "Barangay 2", "Barangay 3", "Barangay 4", "Barangay 5", "Barangay 6", "Barangay 7", 
+        "Barangay 8", "Barangay 9", "Barangay 10", "Barangay 11", "Barangay 12", "Barangay 13", "Barangay 14",
+        "Barangay 15", "Barangay 16", "Barangay 17", "Barangay 18", "Barangay 19", "Barangay 20", "Barangay 21",
+        "Barangay 22", "Barangay 23", "Barangay 24", "Barangay 25", "Barangay 26", "Barangay 27", "Barangay 28",
+        "Barangay 29", "Barangay 30", "Barangay 31", "Barangay 32", "Barangay 33", "Barangay 34", "Barangay 35",
+        "Barangay 36", "Barangay 37", "Barangay 38", "Barangay 39", "Barangay 40", "Barangay 41", "Barangay 42",
+        "Barangay 43", "Barangay 44", "Barangay 45", "Barangay 46", "Barangay 47", "Barangay 48", "Barangay 49",
+        "Barangay 50", "Barangay 51", "Barangay 52", "Barangay 53", "Barangay 54", "Barangay 55", "Barangay 56",
+        "Barangay 57", "Barangay 58", "Barangay 59", "Barangay 60", "Barangay 61", "Barangay 62", "Barangay 63", 
+        "Barangay 64", "Barangay 65", "Barangay 66", "Barangay 67", "Barangay 68", "Barangay 69", "Barangay 70",
+        "Barangay 71", "Barangay 72", "Barangay 73", "Barangay 74", "Barangay 75", "Barangay 76", "Barangay 77", 
+        "Barangay 78"
+    ],
+    "Trece Martires": [
+        "Aguado", "Cabezas", "Cabuco", "Conchu", "De Ocampo", "Gregorio", "Inocencio", "Lallana", "Lapidario",
+        "Luciano", "Osorio", "Perez", "San Agustin", "Torres", "Vicat"
+    ],
+    "Tagaytay": [
+        "Asisan", "Bagong Tubig", "Dapdap East", "Dapdap West", "Francisco", "Guinhawa North", "Guinhawa South",
+        "Kaybagal Central", "Kaybagal North", "Kaybagal South", "Mag-asawang Ilat", "Maharlika East", "Maharlika West", "Mataas na Kahoy", "Norzagaray", "Pampang", "San Jose", "San Luis", "San Rafael", "Santo Niño", "Tagaytay Proper" ], 
+        
+    "Silang": [ "Aguinaldo", "Aguinaldo Highway", "Bayan Luma I", "Bayan Luma II", "Bayan Luma III", "Bayan Luma IV", "Buan", "Dela Cruz", "Guadalupe", "Guadalupe Heights", "Indang", "Maguigingay", "Mangalang", "Matiyaga", "Mendez", "Mendez Crossing", "Pansol", "Pinagtipunan", "Poblacion I", "Poblacion II", "Poblacion III", "San Antonio", "San Juan", "San Roque", "San Vicente", "Sapang I", "Sapang II", "Sapang III", "Talon I", "Talon II", "Talon III", "Talon IV", "Talon V", "Talon VI" ], 
+    
+    "Gen. Emilio Aguinaldo": [ "Bagong Silang", "Bayabas", "Kaytitinga", "Longos", "Maligaya", "Manila", "Mansalay", "Maya", "San Francisco", "San Juan", "Santa Cruz", "Santa Maria", "Talisay" ], 
+    
+    "Alfonso": [ "Aguinaldo", "Biga", "Buhay na Tubig", "Dapdap", "Kayquit", "Manggahan", "Poblacion", "San Jose", "San Juan", "San Vicente", "Talon" ], 
+    
+    "Amadeo": [ "Bacong", "Bagong Silang", "Balanoy", "Cagubatan", "Carmen", "Garnet", "Limao", "Mahayag", "Malhacan", "Marangay", "Minite", "Parang", "San Antonio", "San Carlos", "San Jose", "San Pedro", "Silang", "Tinungtungan" ], 
+    
+    "Carmona": [ "Baclaran", "Bihang", "Carmona Heights", "Carmona South", "Carmona West", "Cotcot", "Loma De Gato", "San Agustin", "San Jose", "San Pedro", "San Vicente", "Santo Domingo" ], 
+    
+    "Gen. Mariano Alvarez": [ "Amaya", "Bantay", "Bata", "Carmona", "Dela Torre", "Dela Torre III", "Indang", "Jaguin", "Ligas", "Longos", "Poblacion", "San Jose", "San Pedro", "Santo Tomas", "Santa Teresa" ] };  
+
+window.onload = function(){
+    const selectCity = document.getElementById('city'),
+        selectBarangay = document.getElementById('barangay'),
+        selects = document.querySelectorAll('select')
+
+        selectBarangay.disabled = true;
+
+        selects.forEach(select => {
+            if (select.disabled == true) {
+                select.style.cursor = "auto"
+            }
+        })
+
+        for (let city in cityBarangay) {
+            selectCity.options[selectCity.options.length] = new Option(city, city);
+        }
+
+        selectCity.onchange = (e) => {
+            selectBarangay.disabled = false;
+
+            selectBarangay.length = 1;
+
+            let barangays = cityBarangay[e.target.value];
+
+            for (let barangay of barangays) {
+                selectBarangay.options[selectBarangay.options.length] = new Option(barangay, barangay);
+            }
+        }
+
+}
